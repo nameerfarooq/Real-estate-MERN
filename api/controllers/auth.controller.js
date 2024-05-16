42504eca58cdb7062c70c0c955b125456ed277fb
Nameerfarooq <nameerfarooquit@gmail.com>
2024-05-16 17:01:07 +0500

    create signup api

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+import { json } from "express";
+import userModel from "../models/user.model.js";
+import bycryptjs from "bcryptjs";
+export const signUp = async (req, res) => {
+  console.log(req.body);
+  try {
+    const { username, email, password } = req.body;
+    const hashedPassword = bycryptjs.hashSync(password, 10);
+    const newUser = await userModel.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    res.status(201).json("User created successfully");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message);
+  }
+};
