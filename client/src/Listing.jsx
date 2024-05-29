bda000bdb0c100fe6ec40f087f82c89def30690b
Nameerfarooq <nameerfarooquit@gmail.com>
2024-05-29 17:36:23 +0500

    show listing page in progress

--- client/src/Listing.jsx
+++ client/src/Listing.jsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
+import { FaMapMarked, FaMapMarker, FaMarker } from "react-icons/fa";
 const Listing = () => {
   SwiperCore.use([Navigation]);
   const [error, setError] = useState(false);
@@ -53,6 +54,30 @@ const Listing = () => {
               </SwiperSlide>
             ))}
           </Swiper>
+          <div className="p-3 max-w-7xl mx-auto ">
+            <div className="text-3xl font-semibold my-7">
+              <span>{listing.name}</span> - $<span>{listing.regularPrice}</span>{" "}
+              {listing.type === "rent" && <span>/ month</span>}
+            </div>
+            <div className="flex gap-2 items-center">
+              <FaMapMarked color="green" />
+              <span>{listing.address}</span>
+            </div>
+            <div className="flex gap-4 items-center my-4">
+              <div className="p-1 rounded-lg  w-32 text-center  bg-red-900 text-white">
+                For {listing.type}
+              </div>
+              {listing.offer && (
+                <div className="p-1 rounded-lg  w-32 text-center  bg-green-900 text-white">
+                  ${listing.discountPrice}
+                </div>
+              )}
+            </div>
+            <p className="text-slate-800 my-8">
+              <span className="font-semibold text-black">Description - </span>
+              {listing.description}
+            </p>
+          </div>
         </>
       )}
     </main>
